b534ae3a40bd0c975203dbfb9b4fcb9cf443cc3a
itamar <itamarlaufer1@gmail.com>
2020-10-07 21:06:52 +0300

    for each request call, add anregister an api call instance to the collector

--- silk/request.py
+++ silk/request.py
@@ -1,9 +1,9 @@
+import json as json_module
 import traceback
 
 from django.utils import timezone
 
 from silk.collector import DataCollector
-from silk.sql import _should_wrap
 
 
 def wrapped_request(self, method, url,
@@ -11,24 +11,25 @@ def wrapped_request(self, method, url,
                     auth=None, timeout=None, allow_redirects=True, proxies=None,
                     hooks=None, stream=None, verify=None, cert=None, json=None):
     tb = ''.join(reversed(traceback.format_stack()))
-    sql_query = {'method': method, 'url': url, 'params': params, 'json': json}
-    if _should_wrap(sql_query):
-        query_dict = {
-            'query': str(sql_query),
-            'start_time': timezone.now(),
-            'traceback': tb
-        }
-        try:
-            return self._request(method, url, params, data, headers, cookies, files, auth, timeout, allow_redirects,
-                                 proxies, hooks, stream, verify, cert, json)
-        finally:
-            query_dict['end_time'] = timezone.now()
-            request = DataCollector().request
-            if request:
-                query_dict['request'] = request
-            # if self.query.model.__module__ != 'silk.models':
-            DataCollector().register_query(query_dict)
-            # else:
-            #     DataCollector().register_silk_query(query_dict)
-    return self._request(method, url, params, data, headers, cookies, files, auth, timeout, allow_redirects,
-                         proxies, hooks, stream, verify, cert, json)
+    api_call_dict = {'method': method, 'url': url, 'query_params': json_module.dumps({'param': params, 'json': json}),
+                     'traceback': tb, 'start_time': timezone.now()}
+    sql_query = {'method': method, 'url': url, 'query_params': json_module.dumps({'param': params, 'json': json})}
+    query_dict = {
+        'query': str(sql_query),
+        'start_time': timezone.now(),
+        'traceback': tb
+    }
+    try:
+        return self._request(method, url, params, data, headers, cookies, files, auth, timeout, allow_redirects,
+                             proxies, hooks, stream, verify, cert, json)
+    finally:
+        api_call_dict['end_time'] = timezone.now()
+        query_dict['end_time'] = timezone.now()
+        request = DataCollector().request
+        if request:
+            api_call_dict['request'] = request
+            query_dict['request'] = request
+        DataCollector().register_api_call(api_call_dict)
+        DataCollector().register_query(query_dict)
+        return self._request(method, url, params, data, headers, cookies, files, auth, timeout, allow_redirects,
+                             proxies, hooks, stream, verify, cert, json)

8f010b35626555b8257c113bb2ea57ed29414488
itamar <itamarlaufer1@gmail.com>
2020-10-08 12:18:26 +0300

    update the sturcute of the APICall query_params

--- silk/request.py
+++ silk/request.py
@@ -11,14 +11,16 @@ def wrapped_request(self, method, url,
                     auth=None, timeout=None, allow_redirects=True, proxies=None,
                     hooks=None, stream=None, verify=None, cert=None, json=None):
     tb = ''.join(reversed(traceback.format_stack()))
-    api_call_dict = {'method': method, 'url': url, 'query_params': json_module.dumps({'param': params, 'json': json}),
-                     'traceback': tb, 'start_time': timezone.now()}
-    sql_query = {'method': method, 'url': url, 'query_params': json_module.dumps({'param': params, 'json': json})}
-    query_dict = {
-        'query': str(sql_query),
-        'start_time': timezone.now(),
-        'traceback': tb
-    }
+    api_call_dict = {'method': method.upper(), 'url': url, 'traceback': tb, 'start_time': timezone.now()}
+
+    query_params = {}
+    if json:
+        query_params['json'] = json
+    if params:
+        query_params['params'] = params
+    api_call_dict['query_params'] = json_module.dumps(query_params)
+
+    response = None
     try:
         return self._request(method, url, params, data, headers, cookies, files, auth, timeout, allow_redirects,
                              proxies, hooks, stream, verify, cert, json)
